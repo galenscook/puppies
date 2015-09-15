d6120ceb4e5ff75d48793c0f95d03ab0019c7b85
Galen Cook <galenscook@gmail.com>
2015-09-15 14:59:27 -0700

    Add ajax functionality for hearts

--- app/controllers/hearts.rb
+++ app/controllers/hearts.rb
@@ -1,15 +1,29 @@
 post '/hearts' do
-  heart = Heart.create(params[:heart])
-  @errors = heart.errors.full_messages
-  redirect "/photos/#{params[:heart][:photo_id]}"
+  @heart = Heart.create(params[:heart])
+  @errors = @heart.errors.full_messages
+  @photo = Photo.find(@heart.photo_id)
+  heart_info = {heart_count: @photo.heart_count.to_s, comment: @heart.comment}
+  if request.xhr? 
+    heart_info.to_json
+  else
+    redirect "/photos/#{params[:heart][:photo_id]}"
+  end
 end
 
+#
 put '/hearts' do
 
 end
 
 delete '/hearts' do
-  heart = Heart.find_by(user_id: params[:user_id], photo_id: params[:photo_id])
-  heart.destroy
-  redirect "/photos/#{params[:photo_id]}"
+  @heart = Heart.find_by(user_id: params[:user_id], photo_id: params[:photo_id])
+  @comment = @heart.comment.to_s
+  @heart.destroy
+  @photo = Photo.find(@heart.photo_id)
+  heart_info = {heart_count: @photo.heart_count.to_s, heart: :'/hearts/_heart', comment: @comment}
+  if request.xhr?
+    heart_info.to_json
+  else
+    redirect "/photos/#{params[:photo_id]}"
+  end
 end
--- public/js/application.js
+++ public/js/application.js
@@ -4,4 +4,66 @@ $(document).ready(function() {
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  heart();
+  unheart();
 });
+
+
+function heart() {
+  $('#heart').submit(function(event){
+    event.preventDefault();
+
+    var heartInfo = $(this).serialize()
+
+    $.ajax({
+      method:"post",
+      url: "/hearts",
+      data: heartInfo,
+      //datatype: "json"
+    })
+
+    .done(function(response) {
+      response = JSON.parse(response);
+      $('.heart_count').text("Heart Count = "+response["heart_count"]);
+      $('.comments').append('<li>'+response.comment+'</li>');
+      $('#heart').hide();
+      $('#unheart.hidden').show();
+    })
+
+    .fail(function(response){
+      console.log("FAIL")
+      console.log(response)
+    })
+  })
+};
+
+function unheart() {
+  $('#unheart').submit(function(event){
+    event.preventDefault();
+
+    var heartInfo = $(this).serialize()
+
+    $.ajax({
+      method: "post",
+      url: "/hearts",
+      data: heartInfo
+    })
+
+    .done(function(response){
+      response = JSON.parse(response);
+      console.log(response.comment);
+      $('.heart_count').text("Heart Count = "+response["heart_count"]);
+      $('<li>'+response.comment+'</li>').remove();
+      $('#unheart').hide();
+      $('#heart.hidden').show();
+    })
+
+    .fail(function(response){
+      console.log("FAIL")
+      console.log(response)
+    })
+
+  });
+
+
+}
