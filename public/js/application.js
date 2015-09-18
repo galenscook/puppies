4265e12443adc7a9dd2fa2e4fc2aed4a4c6b8cab
Galen Cook <galenscook@gmail.com>
2015-09-17 19:42:24 -0700

    Add salvattore base

--- public/js/application.js
+++ public/js/application.js
@@ -7,6 +7,28 @@ $(document).ready(function() {
   heart();
   unheart();
   // photoGrid();
+
+  // function append(title, content) {
+  //     // build/select our elements
+  //     var grid = $('#columns')[0];
+  //     var item = document.createElement('div');
+  //     // build the html
+  //     var h = '<div class="panel panel-primary">';
+  //     h += '<div class="panel-heading">';
+  //     h += title;
+  //     h += '</div>';
+  //     h += '<div class="panel-body">';
+  //     h += content;
+  //     h += '</div>';
+  //     h += '</div>';
+  //     salvattore['append_elements'](grid, [item])
+  //     item.outerHTML = h;
+  // }
+  // $.getJSON("https://www.googleapis.com/books/v1/volumes?q=inauthor:Ernest+Hemingway&callback=?", function (data) {
+  //     $(data.items).each(function (i, book) {
+  //         append(book.volumeInfo.title, book.volumeInfo.description);
+  //     });
+  // });
 });
 
 
@@ -75,7 +97,7 @@ function photoGrid () {
     itemSelector: '.grid-item',
     columnWidth: '.grid-sizer',
     percentPosition: true,
-    gutter: 40,
+    gutter: 10,
   });
 
 }
